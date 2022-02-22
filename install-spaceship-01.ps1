3919b04c7b7c36fbc491e4c94552fb2586621085
Joan Comas <joan.comas.fdz@gmail.com>
2022-02-22 11:35:24 +0100

    Added powershell scripts

---
+++ install-spaceship-01.ps1
@@ -0,0 +1,9 @@
+Write-Host "========================================="
+Write-Host "Installing WSL..."
+Write-Host "========================================="
+wsl --install
+
+Write-Host
+Write-Host "========================================="
+Write-Host "Please Reboot, then run install-spaceship-02.ps1 as Admin."
+Write-Host "========================================="
